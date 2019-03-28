0fbe6debbcfa1b1b029a317f3fea09bd7ffd0e9e
MrTBit <timjbart97@gmail.com>
2019-03-28 19:53:32 -0400

    typo

--- main.go
+++ main.go
@@ -23,12 +23,9 @@ func main() {
 	router.HandleFunc("/api/boards/{boardID}/threads/{threadID}/posts", controllers.GetCommentsFor).Methods("GET")
 	router.HandleFunc("/api/boards/{boardID}/threads/{threadID}/posts/{postID}", controllers.GetComment).Methods("GET")
 
-	//router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
-	//router.HandleFunc("/api/user/{id}/contacts", controllers.GetContactsFor).Methods("GET") // user/2/contacts
-
 	router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
-	port := os.Getenv("PORT") //get port from .env, we didn't speicfy so it will return empty string
+	port := os.Getenv("PORT") //get port from .env, we didn't specify so it will return empty string
 	if port == "" {
 		port = "8000" //localhost
 	}
